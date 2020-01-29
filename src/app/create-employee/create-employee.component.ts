36db23a37afcc750b6e779a211411614f020fd12
valdy <valdy@xxx.com>
2020-01-29 10:40:36 +0700

    First Commit

---
+++ src/app/create-employee/create-employee.component.ts
@@ -0,0 +1,49 @@
+import { EmployeeService } from '../employee.service';
+import { Employee } from '../employee';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { setTimeout } from 'timers';
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent implements OnInit {
+
+  employee: Employee = new Employee();
+  submitted = false;
+  loading = false;
+
+
+  constructor(private employeeService: EmployeeService,
+    private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  newEmployee(): void {
+    this.submitted = false;
+    this.employee = new Employee();
+  }
+
+  //SAVE
+  save() {
+    this.employeeService.createEmployee(this.employee)
+      .subscribe(data => console.log(data), error => console.log(error));
+    this.employee = new Employee();
+    this.gotoList();
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.save();
+  }
+
+  gotoList() {
+    setTimeout(() => {
+      this.router.navigate(['/employees']);
+    }, 1000);
+  }
+
+}
---
+++ src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { EmployeeDetailsComponent } from '../employee-details/employee-details.component';
+import { Observable } from "rxjs";
+import { EmployeeService } from "../employee.service";
+import { Employee } from "../employee";
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+  employees: Observable<Employee[]>;
+
+  constructor(private employeeService: EmployeeService,
+    private router: Router) {}
+
+  ngOnInit() {
+    this.reloadData();
+  }
+
+  reloadData() {
+    this.employees = this.employeeService.getEmployeesList();
+    console.log(this.employees);
+  }
+
+  //DELETE EMPLOYEE
+  deleteEmployee(id: number) {
+    this.employeeService.deleteEmployee(id)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.reloadData();
+        },
+        error => console.log(error));
+  }
+
+  //DETAIL EMPLOYEE
+  employeeDetails(id: number){
+    this.router.navigate(['details', id]);
+  }
+
+  //UPDATE EMPLOYEE
+  employeeUpdate(id: number){
+    this.router.navigate(['update', id]);
+  }
+
+}
